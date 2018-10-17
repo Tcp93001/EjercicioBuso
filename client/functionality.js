0a85530e5aa9eee684e8e3d5ba6a45b75c8fa574
Tcp93001 <joseguerreroroman@yahoo.com.mx>
2018-10-17 11:17:34 -0500

    Add functionality to read and set new routes.

--- client/functionality.js
+++ client/functionality.js
@@ -1,23 +1,76 @@
+
+const urlBus = 'https://0sysjslkra.execute-api.us-east-1.amazonaws.com/test/routes';
+const headersSent = {
+  'Content-Type':'application/json',
+  'Accept': 'application/json',
+  'x-api-key': 'Yrk3AZ1yOT7PIBbWJNrkB541cLBnff5w6cSZH9qr'
+};
+
 $( document ).ready(function() {
   retrieveRouteData()
 });
 
-function createRoute() {
-  console.log("ready");
+function delete_route() {
+  $('#deleteModal').modal('toggle')
+  let urlBusDelete = urlBus + '/' + $('#deleteRouteId').val()
+  // $.ajax({
+  //   url: urlBusDelete,
+  //   headers: headersSent,
+  //   method: 'DELETE',
+  //   dataType: 'json',
+  //   success: function(data){
+    //     console.log(data)
+  //     $("#inicioDeTabla").empty()
+  //     retrieveRouteData();
+  //   },
+  //   error: function(err){
+    //     console.log(err)
+  //   }
+  // });
 }
 
-function delete_route() {
-  console.log("ok");
+function createRoute() {
+  let newRouteInformation = {
+    "id": $("#createFormId").val(),
+    "origin_id": $("#createFormOrigin").val(),
+    "destination_id": $("#createFormDestination").val(),
+    "departure_time": $("#createFormDeparture").val(),
+    "arrival_time": $("#createFormArrival").val(),
+    "company_name": $("#createFormCompany").val()
+  }
+  $('#createModal').modal('toggle')
+  console.log("sent sent sent");
+  // $.ajax({
+  //   url: urlBus,
+  //   headers: headersSent,
+  //   data: JSON.stringify(newRouteInformation),
+  //   method: 'POST',
+  //   dataType: 'json',
+  //   success: function(data){
+    //     console.log(data)
+  //     $("#inicioDeTabla").empty()
+  //     retrieveRouteData();
+  //   },
+  //   error: function(err){
+    //     console.log(err)
+  //   }
+  // });
+  resetValuesModal();
+}
+
+function resetValuesModal() {
+  $("#createFormId").val("")
+  $("#createFormOrigin").val("");
+  $("#createFormDestination").val("");
+  $("#createFormDeparture").val("");
+  $("#createFormArrival").val("");
+  $("#createFormCompany").val("");
 }
 
 function retrieveRouteData() {
   $.ajax({
-    url: 'https://0sysjslkra.execute-api.us-east-1.amazonaws.com/test/routes',
-    headers: {
-      'Content-Type':'application/json',
-      'Accept': 'application/json',
-      'x-api-key': 'Yrk3AZ1yOT7PIBbWJNrkB541cLBnff5w6cSZH9qr'
-    },
+    url: urlBus,
+    headers: headersSent,
     method: 'GET',
     dataType: 'json',
     success: function(data){
@@ -33,7 +86,8 @@ function retrieveRouteData() {
 
 function makingTable(elem, index) {
   let secuence = index + 1;
-  elem.id = ((elem.id).length == 1) ? elem.id = "Sin registro" : elem.id
+  console.log(elem.id)
+  elem.id = (elem.id === "[object Undefined]") ? elem.id = "Sin registro" : elem.id
   let step = $('#inicioDeTabla');
   let tableContent = '<tr><th scope="row">' + secuence + '</th>';
   tableContent += '<td>' + elem.id + '</td>';
@@ -44,3 +98,4 @@ function makingTable(elem, index) {
   tableContent += '<td>' + elem.destination_id + '</td>';
   step.append(tableContent);
 }
+
